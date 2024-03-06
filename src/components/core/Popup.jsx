fe3ba5c03e33d19349d405d2e8e7c13e83561b65
Zeazelu <zeazelu@gmail.com>
2024-03-06 11:39:03 +0100

    first commit

---
+++ src/components/core/Popup.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import MyButton from '../util/MyButton.jsx';
+import Fade from 'react-reveal/Fade';
+class Popup extends Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            time: 'start',
+            title: 'Witamy w teście rekrutacyjnym na ambasadora WEBTECH SOLUTIONS',
+            text: 'Do rowzwiązania nie używaj Internetu ani ChatGPT <br /><br />',
+            buttonText: 'Rozpocznij test' 
+        };
+        
+        this.popupHandle = this.popupHandle.bind(this);
+    }
+    
+    popupHandle() {
+        let { time } = this.state;
+        
+        if(time === 'start'){
+            this.setState({
+                time: 'end',
+                title: 'Gratulacje!',
+                buttonText: 'Spróbuj jeszcze raz'
+            });
+
+            //alert("START THE QUIZ");
+            this.props.startQuiz();
+        } else {
+            
+            //alert("FINISHED QUIZ");            
+            location.reload();// restart the application
+        }
+    }
+     
+    createMarkup(text) {
+        return {__html: text};
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            text: 'Koniec testu. <br /> Otrzymałeś: <strong>' + this.props.score + 
+            '</strong> na <strong>' + 
+            this.props.total +
+            '</strong> pytań.'
+        })
+    }
+
+    
+    render() {
+       
+        let { title, text, buttonText } = this.state;
+        
+        let { style } = this.props;
+        
+        return (
+            <Fade delay={500}>
+                <div className="popup-container" style={style}>
+                    <div className="container">
+                        <div className="ml-5 col-md-10 col-10">
+                            <div className="popup">
+                                <h1>{title}</h1>
+                                <p dangerouslySetInnerHTML={this.createMarkup(text)} />
+                                <span onClick={this.popupHandle}>
+                                    <MyButton
+                                        text={buttonText}
+                                        bck='#FF9800'
+                                        color='#fff'
+                                    />
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Fade>
+        );
+    }
+}
+
+export default Popup; 
+
---
+++ src/components/questions_data/data.js
@@ -0,0 +1,242 @@
+export default [
+    {
+        question: 'Dlaczego właściciel WEBTECH SOLUTIONS urwał kontakt z Joanną Opozdą?',
+        answers: [
+            'Bo jest psychiczna',
+            'Zawsze narzekała na kwiaty',
+            'Była bardzo zazdrosna',
+            'Męczyła bułę'
+        ],
+        correct: 1
+    },
+
+    {
+        question: 'Jak właściciel WEBTECH SOLUTIONS obniża poziom cholesterolu?',
+        answers: [
+            'Chudnie 5 kilo',
+            'Bierze leki',
+            'Nie obniża',
+            'Już obniżył'
+        ],
+        correct: 3
+    },    
+    {
+        question: 'Ile produktów w gamie ma WEBTECH SOLUTIONS?',
+        answers: [
+            '3',
+            '4', 
+            '5', 
+            '6'
+        ],    
+        correct: 2
+    },    
+    {
+        question: 'Ilu ambasadorów pracuje w WEBTECH SOLUTIONS na stałe?',
+        answers: [
+            '3',
+            '4', 
+            'Nie ma ambasadorów', 
+            '1'
+        ],    
+        correct: 1
+    },
+    {
+        question: 'Na jakie danie nie mógł sobie pozwolić właściciel WEBTECH SOLUTIONS zanim został biznesmenem?',
+        answers: [
+            'Ramen',
+            'Żeberka', 
+            'Stek', 
+            'Sushi'
+        ],    
+        correct: 2
+    },
+    {
+        question: 'Jaką niespodziankę znalazły dziewczyny po nocy w Gdańsku?',
+        answers: [
+            'Zużytą prezerwatywę',
+            'Kostkę z pałeczki z kurczaka', 
+            'Śmierdzącą skarpetkę', 
+            'Wymioty'
+        ],    
+        correct: 2
+    },
+    {
+        question: 'Co głównie wyróżnia biuro WEBTECH SOLTIONS?',
+        answers: [
+            'Ergonomiczne stanowisko pracy',
+            'Duża ilość sprzętu do pracy', 
+            'Tablet z rysikiem', 
+            'Majtki na środku biura'
+        ],    
+        correct: 4
+    },
+    {
+        question: 'Czym zostało wygłuszone biuro WEBTECH SOLUTIONS?',
+        answers: [
+            'Ręcznikiem',
+            'Koszulką', 
+            'Spodniami', 
+            'Wszystkie odpowiedzi są prawidłowe'
+        ],    
+        correct: 4
+    },
+    {
+        question: 'Co można robić w aucie firmowym WEBTECH SOLUTIONS?',
+        answers: [
+            'Jeść',
+            'Palić', 
+            'Ganiać się', 
+            'Przewozić meble'
+        ],    
+        correct: 3
+    },
+    {
+        question: 'W czym właściciel WEBTECH SOLUTIONS pójdzie do ślubu?',
+        answers: [
+            'Koszulka Tommy Hilfiger',
+            'Bluza Calvin Klein', 
+            'Garnitur', 
+            'Odpowiedzi A i B są prawidłowe'
+        ],    
+        correct: 4
+    },
+    {
+        question: 'Co można robić z właścicielem WEBTECH SOLUTIONS?',
+        answers: [
+            'Zmotać się na jakieś bro',
+            'Normalnie porozmawiać', 
+            'Pójść na spacer', 
+            'Pójść na zakupy'
+        ],    
+        correct: 1
+    },
+    {
+        question: 'Jakie jest popisowe danie WEBTECH SOLUTIONS?',
+        answers: [
+            'Hot-dog',
+            'Tosty', 
+            'Ryż z kurczakiem', 
+            'Zapiekanka'
+        ],    
+        correct: 4
+    },
+    {
+        question: 'Jaki sposób zapłaty najczęściej wybiera właściciel WEBTECH SOLUTIONS?',
+        answers: [
+            'Zegarek',
+            'Gotówka', 
+            'Karta', 
+            'Blik'
+        ],    
+        correct: 1
+    },
+    {
+        question: 'Jak podchodzi do życia właściciel WEBTECH SOLUTIONS?',
+        answers: [
+            'Ambitnie',
+            'Na spokojnie', 
+            'Leniwie', 
+            'Odważnie'
+        ],    
+        correct: 2
+    },
+    {
+        question: 'Czym ekipa WEBTECH SOLUTIONS zwlaczała wilgość?',
+        answers: [
+            'Ryżem',
+            'Gąbką', 
+            'Pochłaniaczem wilgoci', 
+            'Ścierką'
+        ],    
+        correct: 1
+    },
+    {
+        question: 'Kto z ekipy WEBTECH SOLUTIONS zajmuje się regulacją rolet?',
+        answers: [
+            'Artur',
+            'Karol', 
+            'Danielek', 
+            'Mariusz'
+        ],    
+        correct: 4
+    },
+    {
+        question: 'Dlaczego lis z Władysławowa nie doniósł Vansa do nory?',
+        answers: [
+            'Stwierdził że nie ma sensu',
+            'Po drodze zabrał mu go inny lis', 
+            'Nie mógł wytrzymać bo tak śmierdział', 
+            'Zgubił, bo sznurówka się urwała'
+        ],    
+        correct: 3
+    },
+    {
+        question: 'Jaki jest najlepszy smak GBS?',
+        answers: [
+            'Biała czekolada z kokosem',
+            'Karmel', 
+            'Ciasteczkowy', 
+            'Rumowy'
+        ],    
+        correct: 4
+    },
+    {
+        question: 'Który tytuł piosenki jest zapisany poprawnie?',
+        answers: [
+            'Icu picu',
+            'Itsu pitsu', 
+            'Icupicu', 
+            'Icu pitcu'
+        ],    
+        correct: 2
+    },
+    {
+        question: 'Jaką dodatkową funkcję posiadają słuchawki ambasadorek?',
+        answers: [
+            'Odpowiedzi C i D są poprawne',
+            'Redukcja hałasu', 
+            'Redukcja słuchu', 
+            'Nie trzeba ich ładować'
+        ],    
+        correct: 1
+    },
+    {
+        question: 'Toyota Yaris Verso jest autem:',
+        answers: [
+            'Sportowym',
+            'Luksusowym', 
+            'Praktycznym', 
+            'Wszystkie odpowiedzi są poprawne'
+        ],    
+        correct: 4
+    },
+    {
+        question: 'Jakie przezwisko miały buty wyjściowe właściciela WEBTECH SOLUTIONS?',
+        answers: [
+            'Maxy',
+            'Żółtki', 
+            'Tryple', 
+            'Trzewiki'
+        ],    
+        correct: 2
+    },
+    {
+        question: 'Dlaczego właściciel WEBTECH SOLUTIONS bierze wszystko na fakturę?',
+        answers: [
+            'Bo wtedy jest za darmo',
+            'Popisuje się', 
+            'Wszystkie odpowiedzi są błędne', 
+            'Odpowiedzi A i B są prawidłowe'
+        ],    
+        correct: 4
+    },
+    {
+        question: 'Oceń stwierdzenie: Właściciel WEBTECH SOLUTIONS robi z siebie większego błazna niż jest.',
+        answers: [
+            'Prawda',
+            'Fałsz',
+        ],    
+        correct: 1
+    },
+    
+]
