a83a64f741942ddd261a6ada74556a73f7ee8f86
Zeazelu <zeazelu@gmail.com>
2024-03-07 14:17:55 +0100

    popup fix

--- src/components/core/Popup.jsx
+++ src/components/core/Popup.jsx
@@ -1,61 +1,82 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import MyButton from '../util/MyButton.jsx';
 import Fade from 'react-reveal/Fade';
-
-const Popup = (props) => {
-    const [state, setState] = useState({
-        time: 'start',
-        title: 'Witamy w teście rekrutacyjnym na stanowisko - Ambasador WEBTECH SOLUTIONS',
-        text: 'Do rozwiązania testu nie używaj Internetu ani ChatGPT <br /><br />',
-        buttonText: 'Rozpocznij test'
-    });
-
-    const { startQuiz, score, total, style } = props;
-
-    const popupHandle = () => {
-        const { time } = state;
-
-        if (time === 'start') {
-            setState({
+class Popup extends Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            time: 'start',
+            title: 'Test rekrutacyjny na stanowisko - Ambasador WEBTECH SOLUTIONS',
+            text: 'Rozwiązuj test samodzielnie. <br /><br />',
+            buttonText: 'Rozpocznij test' 
+        };
+        
+        this.popupHandle = this.popupHandle.bind(this);
+    }
+    
+    popupHandle() {
+        let { time } = this.state;
+        
+        if(time === 'start'){
+            this.setState({
                 time: 'end',
                 title: 'Dziękujemy za rozwiązanie testu!',
-                buttonText: 'Spróbuj jeszcze raz'
+                buttonText: 'Rozwiąż ponownie'
             });
 
-            startQuiz();
+            //alert("START THE QUIZ");
+            this.props.startQuiz();
         } else {
-            location.reload(); // restart the application
+            
+            //alert("FINISHED QUIZ");            
+            location.reload();// restart the application
         }
-    };
+    }
+     
+    createMarkup(text) {
+        return {__html: text};
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            text: 'Koniec testu. <br /> Zdobyłeś <strong>' + this.props.score + 
+            '</strong> z <strong>' + 
+            this.props.total +
+            '</strong> punktów.'
+        })
+    }
 
-    const createMarkup = (text) => ({ __html: text });
-
-    useEffect(() => {
-        setState({
-            ...state,
-            text: `Koniec testu. <br /> Otrzymałeś: <strong>${score}</strong> na <strong>${total}</strong> puntków.`
-        });
-    }, [score, total]);
-
-    const { title, text, buttonText } = state;
-
-    return (
-        <Fade delay={500}>
-            <div className="popup-container text-center" style={style}>
-                <div className="container">
-                    <div className="col-md-10 col-12 mx-auto">
-                        <div className="popup">
-                            <h1>{title}</h1>
-                            <p dangerouslySetInnerHTML={createMarkup(text)} />
-                            <span onClick={popupHandle}>
-                                <MyButton text={buttonText} bck='#0d0d0e' color='#fff' />
-                            </span>
+    
+    render() {
+       
+        let { title, text, buttonText } = this.state;
+        
+        let { style } = this.props;
+        
+        return (
+            <Fade delay={500}>
+                <div className="popup-container" style={style}>
+                    <div className="container">
+                        <div className="ml-5 col-md-10 col-10">
+                            <div className="popup">
+                                <h1>{title}</h1>
+                                <p dangerouslySetInnerHTML={this.createMarkup(text)} />
+                                <span onClick={this.popupHandle}>
+                                    <MyButton
+                                        text={buttonText}
+                                        bck='#0d0d0e'
+                                        color='#fff'
+                                    />
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </Fade>
-    );
-};
+            </Fade>
+        );
+    }
+}
 
-export default Popup;
+export default Popup; 
