a79cabb324de41e79d6d5290794c587df0acefbe
Zeazelu <zeazelu@gmail.com>
2024-03-13 13:34:29 +0100

    add password to begin test

--- src/components/core/Popup.jsx
+++ src/components/core/Popup.jsx
@@ -2,60 +2,69 @@ import React, { Component } from 'react';
 import MyButton from '../util/MyButton.jsx';
 import Fade from 'react-reveal/Fade';
 import './Popup.css'
+
 class Popup extends Component {
-    
     constructor(props) {
         super(props);
-        
+
         this.state = {
             time: 'start',
             title: 'Test rekrutacyjny na stanowisko - Ambasador WEBTECH SOLUTIONS',
             text: 'Rozwiązuj test samodzielnie!',
-            buttonText: 'Rozpocznij test' 
+            buttonText: 'Rozpocznij test',
+            passwordInput: '', // Dodajemy pole dla wpisywanego hasła
+            passwordError: false // Dodajemy pole do śledzenia błędu hasła
         };
-        
-        this.popupHandle = this.popupHandle.bind(this);
-    }
-    
-    popupHandle() {
-        let { time } = this.state;
-        
-        if(time === 'start'){
-            this.setState({
-                time: 'end',
-                title: 'Dziękujemy za rozwiązanie testu!',
-                buttonText: 'Rozwiąż ponownie'
-            });
 
-            //alert("START THE QUIZ");
-            this.props.startQuiz();
+        this.popupHandle = this.popupHandle.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    popupHandle() {
+        const { time, passwordInput } = this.state;
+
+        if (passwordInput === '1234') {
+            if (time === 'start') {
+                this.setState({
+                    time: 'end',
+                    title: 'Dziękujemy za rozwiązanie testu!',
+                    buttonText: 'Rozwiąż ponownie',
+                    passwordError: false // Resetujemy błąd hasła po poprawnym wprowadzeniu
+                });
+
+                this.props.startQuiz();
+            } else {
+                location.reload(); // restart the application
+            }
         } else {
-            
-            //alert("FINISHED QUIZ");            
-            location.reload();// restart the application
+            // Wyświetl błąd, jeśli hasło jest niepoprawne
+            this.setState({ passwordError: true });
         }
     }
-     
-    createMarkup(text) {
-        return {__html: text};
+
+    handlePasswordChange(event) {
+        this.setState({ passwordInput: event.target.value });
     }
-    
+
+    createMarkup(text) {
+        return { __html: text };
+    }
+
     componentWillReceiveProps(nextProps) {
         this.setState({
-            text: 'Koniec testu. <br /> Zdobyłeś <strong>' + this.props.score + 
-            '</strong> z <strong>' + 
-            this.props.total +
-            '</strong> punktów.'
+            text: 'Koniec testu. <br /> Zdobyłeś <strong>' + this.props.score +
+                '</strong> z <strong>' +
+                this.props.total +
+                '</strong> punktów.'
         })
     }
 
-    
     render() {
-       
-        let { title, text, buttonText } = this.state;
-        
+
+        let { title, text, buttonText, passwordError } = this.state;
+
         let { style } = this.props;
-        
+
         return (
             <Fade delay={500}>
                 <div className="popup-container" style={style}>
@@ -64,11 +73,19 @@ class Popup extends Component {
                             <div className="popup">
                                 <h1>{title}</h1>
                                 <p dangerouslySetInnerHTML={this.createMarkup(text)} />
+                                <input
+                                    type="password"
+                                    placeholder="Wprowadź hasło"
+                                    value={this.state.passwordInput}
+                                    onChange={this.handlePasswordChange}
+                                />
+                                {passwordError && <p style={{ color: 'red' }}>Niepoprawne hasło!</p>}
                                 <span onClick={this.popupHandle}>
                                     <MyButton
                                         text={buttonText}
                                         bck='#0d0d0e'
                                         color='#fff'
+                                        disabled={passwordError || !this.state.passwordInput} // Wyłącz przycisk, jeśli hasło jest niepoprawne lub nie wpisano żadnego hasła
                                     />
                                 </span>
                             </div>
@@ -80,4 +97,4 @@ class Popup extends Component {
     }
 }
 
-export default Popup; 
+export default Popup;
